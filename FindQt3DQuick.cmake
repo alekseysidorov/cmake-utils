c30d758d67f37e984188ce338bbe89e005306253
Sergei Lopatin <magist3r@gmail.com>
2012-09-03 15:03:56 +0600

    Replace all tabs

--- FindQt3DQuick.cmake
+++ FindQt3DQuick.cmake
@@ -7,42 +7,42 @@ find_qt_module(Qt3DQuick
 
 ##find Qt3DQuick
 #if(APPLE)
-#	find_library(QT_3DQUICK_LIBRARIES
-#		NAMES Qt3DQuick
-#		HINTS ${QT_LIBRARY_DIR}
-#	)
-#	if(QT_3DQUICK_LIBRARIES)
-#		message(STATUS "Found Qt3DQuick framework: ${QT_3DQUICK_LIBRARIES}")
-#		set(QT_3DQUICK_FOUND true)
-#		list(APPEND QT_LIBRARIES
-#			${QT_3DQUICK_LIBRARIES}
-#		)
-#		set(QT_3DQUICK_INCLUDE_DIR "${QT_3DQUICK_LIBRARIES}/Headers")
-#		list(APPEND QT_INCLUDES
-#			${QT_3DQUICK_INCLUDE_DIR}
-#		)
-#	else()
-#		message(STATUS "Could NOT find Qt3DQuick")
-#	endif()
+#   find_library(QT_3DQUICK_LIBRARIES
+#       NAMES Qt3DQuick
+#       HINTS ${QT_LIBRARY_DIR}
+#   )
+#   if(QT_3DQUICK_LIBRARIES)
+#       message(STATUS "Found Qt3DQuick framework: ${QT_3DQUICK_LIBRARIES}")
+#       set(QT_3DQUICK_FOUND true)
+#       list(APPEND QT_LIBRARIES
+#           ${QT_3DQUICK_LIBRARIES}
+#       )
+#       set(QT_3DQUICK_INCLUDE_DIR "${QT_3DQUICK_LIBRARIES}/Headers")
+#       list(APPEND QT_INCLUDES
+#           ${QT_3DQUICK_INCLUDE_DIR}
+#       )
+#   else()
+#       message(STATUS "Could NOT find Qt3DQuick")
+#   endif()
 #else()
-#	include(FindLibraryWithDebug)
-#	find_path(QT_3DQUICK_INCLUDE_DIR qt3dquickglobal.h PATH ${QT_INCLUDE_DIR}/Qt3DQuick NO_DEFAULT_PATH)
-#	find_library_with_debug(QT_3DQUICK_LIBRARIES
-#					  WIN32_DEBUG_POSTFIX d
-#					  NAMES Qt3DQuick
-#					  HINTS ${QT_LIBRARY_DIR}
-#					  )
+#   include(FindLibraryWithDebug)
+#   find_path(QT_3DQUICK_INCLUDE_DIR qt3dquickglobal.h PATH ${QT_INCLUDE_DIR}/Qt3DQuick NO_DEFAULT_PATH)
+#   find_library_with_debug(QT_3DQUICK_LIBRARIES
+#                     WIN32_DEBUG_POSTFIX d
+#                     NAMES Qt3DQuick
+#                     HINTS ${QT_LIBRARY_DIR}
+#                     )
 
-#	if(QT_3DQUICK_LIBRARIES AND QT_3DQUICK_INCLUDE_DIR)
-#		message(STATUS "Found Qt3DQuick: ${QT_3DQUICK_LIBRARIES}")
-#		set(QT_3DQUICK_FOUND true)
-#		list(APPEND QT_LIBRARIES
-#			${QT_3DQUICK_LIBRARIES}
-#		)
-#		list(APPEND QT_INCLUDES
-#			${QT_3DQUICK_INCLUDE_DIR}
-#		)
-#	else()
-#		message( STATUS "Could NOT find Qt3DQuick")
-#	endif()
+#   if(QT_3DQUICK_LIBRARIES AND QT_3DQUICK_INCLUDE_DIR)
+#       message(STATUS "Found Qt3DQuick: ${QT_3DQUICK_LIBRARIES}")
+#       set(QT_3DQUICK_FOUND true)
+#       list(APPEND QT_LIBRARIES
+#           ${QT_3DQUICK_LIBRARIES}
+#       )
+#       list(APPEND QT_INCLUDES
+#           ${QT_3DQUICK_INCLUDE_DIR}
+#       )
+#   else()
+#       message( STATUS "Could NOT find Qt3DQuick")
+#   endif()
 #endif()
